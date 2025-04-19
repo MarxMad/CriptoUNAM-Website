bdbfa302d3c00266cb61a121efdfa714bf2818c7
MarxMad <gerardo.gerry.pedrizco@gmail.com>
2025-04-18 23:10:11 -0600

    cambiando rutas de imagenes para vercel

--- criptounam/src/constants/images.ts
+++ criptounam/src/constants/images.ts
@@ -1,31 +1,31 @@
 export const IMAGES = {
   // Hero y elementos generales
-  HERO_BG: 'src/assets/images/hero-bg.jpg',
-  LOGO: 'src/assets/images/LogosCriptounamBlanco.svg',
+  HERO_BG: 'public/images/hero-bg.jpg',
+  LOGO: 'public/images/LogosCriptounamBlanco.svg',
   
   // Imágenes de cursos
   CURSOS: {
-    BLOCKCHAIN_BASICS: 'src/assets/images/cursos/1.svg',
-    SMART_CONTRACTS: 'src/assets/images/cursos/2.svg',
-    DEFI: 'src/assets/images/cursos/3.svg',
+    BLOCKCHAIN_BASICS: 'public/images/cursos/1.svg',
+    SMART_CONTRACTS: 'public/images/cursos/2.svg',
+    DEFI: 'public/images/cursos/3.svg',
   },
   
   // Imágenes de comunidad
   COMUNIDAD: {
     EVENTOS: {
-      HACKATHON: '/assets/images/comunidad/hackathon.jpg',
-      MEETUP: '/assets/images/comunidad/meetup.jpg',
+      HACKATHON: '/images/comunidad/hackathon.jpg',
+      MEETUP: '/images/comunidad/meetup.jpg',
     },
     GALLERY: {
-      EVENT1: '/assets/images/comunidad/gallery/event1.jpg',
-      EVENT2: '/assets/images/comunidad/gallery/event2.jpg',
+      EVENT1: '/images/comunidad/gallery/event1.jpg',
+      EVENT2: '/images/comunidad/gallery/event2.jpg',
     }
   },
   
   // Iconos
   ICONS: {
-    TWITTER: '/assets/images/icons/twitter.svg',
-    TELEGRAM: '/assets/images/icons/telegram.svg',
-    DISCORD: '/assets/images/icons/discord.svg',
+    TWITTER: '/images/icons/twitter.svg',
+    TELEGRAM: '/images/icons/telegram.svg',
+    DISCORD: '/images/icons/discord.svg',
   }
 } 
