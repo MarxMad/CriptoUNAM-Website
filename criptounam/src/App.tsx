a72b7cede5fdbce02ff08373095564bfd7f4abed
MarxMad <gerardo.gerry.pedrizco@gmail.com>
2025-09-30 17:21:45 -0600

    fix: remove conflicting event listeners causing 404 redirects

--- criptounam/src/App.tsx
+++ criptounam/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -16,35 +16,6 @@ import { WalletProvider } from './context/WalletContext'
 import './styles/global.css'
 
 const App = () => {
-  // Event listeners globales para los modales de admin
-  useEffect(() => {
-    // Event listener para Newsletter
-    const handleNewsletterModal = () => {
-      window.location.href = '/newsletter?openModal=true';
-    };
-
-    // Event listener para Cursos
-    const handleCursosModal = () => {
-      window.location.href = '/cursos?openModal=true';
-    };
-
-    // Event listener para Comunidad
-    const handleComunidadModal = () => {
-      window.location.href = '/comunidad?openModal=true';
-    };
-
-    // Registrar event listeners
-    window.addEventListener('openNewsletterModal', handleNewsletterModal);
-    window.addEventListener('openCursosModal', handleCursosModal);
-    window.addEventListener('openComunidadModal', handleComunidadModal);
-
-    return () => {
-      window.removeEventListener('openNewsletterModal', handleNewsletterModal);
-      window.removeEventListener('openCursosModal', handleCursosModal);
-      window.removeEventListener('openComunidadModal', handleComunidadModal);
-    };
-  }, []);
-
   return (
     <WalletProvider>
     <Router>
